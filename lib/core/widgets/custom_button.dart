385a4e90a6fc88c7d9d5786dc42787ab2a5e49b7
belal khleel <bkhleel90@gmail.com>
2025-01-12 16:16:23 +0200

    step2 task

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:taskati/core/utils/colors.dart';
 
-class CustomButton extends StatelessWidget{
+class CustomButton extends StatelessWidget {
   final String text;
+  final onPreassed;
+  final double width;
+  Color color = AppColor.primary;
+  Color textColor = AppColor.primaryContainer;
   CustomButton({
-
+    required this.onPreassed,
     required this.text,
-});
+    this.width = 250,
+  });
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 250,
+      width: width,
       height: 45,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onPreassed,
         child: Text(text),
         style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.primary,
-            foregroundColor: AppColor.primaryContainer,
+            backgroundColor: color,
+            foregroundColor: textColor,
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10))),
       ),
--- lib/screens/upload_user_image/upload_user_image.dart
+++ lib/screens/upload_user_image/upload_user_image.dart
@@ -1,35 +1,118 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:taskati/core/utils/colors.dart';
 import 'package:taskati/core/widgets/custom_button.dart';
 
-class UploadUserImage extends StatelessWidget {
-  const UploadUserImage({super.key});
+class UploadUserImage extends StatefulWidget {
+  UploadUserImage({super.key});
+
+  @override
+  State<UploadUserImage> createState() => _UploadUserImageState();
+}
+
+class _UploadUserImageState extends State<UploadUserImage> {
+  final uploadScreenKey = GlobalKey<FormState>();
+  File? image;
+  final ImagePicker picker = ImagePicker();
+
+  void pickImageFromGallery() async {
+// Pick an image.
+    final XFile? _image = await picker.pickImage(source: ImageSource.gallery);
+    if (_image != null) {
+      image = File(_image.path);
+      setState(() {});
+    }
+  }
+
+  void pickImageFromCamera() async {
+// Pick an image.
+    final XFile? _image = await picker.pickImage(source: ImageSource.camera);
+    if (_image != null) {
+      image = File(_image.path);
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 70,
-            ),
-
-            SizedBox(
-              height: 20,
-            ),
-            CustomButton(text:"Upload Image",),
-            SizedBox(
-              height: 20,
-            ),
-            CustomButton(text:"Upload Image",),
-
+    return Form(
+      key: uploadScreenKey,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            TextButton(
+                onPressed: () {
+                  if (uploadScreenKey.currentState!.validate() &&
+                      image != null) {}
+                },
+                child: Text("Done"))
           ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: image != null
+                    ? FileImage(image!) // Use FileImage for local file
+                    : NetworkImage(
+                        "https://cdn-icons-png.flaticon.com/512/149/149071.png"), // Use NetworkImage for URL
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                text: "Upload Image from gallery",
+                onPreassed: pickImageFromGallery,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(
+                text: "Upload Image from camera",
+                onPreassed: pickImageFromCamera,
+              ),
+              Divider(
+                indent: 20,
+                endIndent: 20,
+                color: AppColor.primary,
+                thickness: 4,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter your name";
+                  }
+                  else if(!RegExp('^[A-z][a-z]{2,8}').hasMatch(value)){
+                    return  'Name must start with a capital letter and be at least 3 characters';
+                  }else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter your name",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColor.primary),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColor.error),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: AppColor.primary),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
